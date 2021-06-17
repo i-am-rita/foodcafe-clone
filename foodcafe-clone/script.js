5ff5c41ddea22faaa9faeb23d3708f5d1b539933
Rita <ritauzoma07@gmail.com>
2021-06-17 11:23:26 +0100

    Add files via upload

---
+++ foodcafe-clone/script.js
@@ -0,0 +1,36 @@
+// window.onscroll = function () {
+//   myFunction()
+// }
+
+// var navbar = documen.getElementsByClassName('topnav')
+// var sticky = navbar.offsetTop
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add('sticky')
+//   } else {
+//     navbar.classList.remove('sticky')
+//   }
+// }
+
+var slideIndex = 0
+showSlides()
+
+function showSlides() {
+  var i
+  var slides = document.getElementsByClassName('mySlides')
+  var dots = document.getElementsByClassName('dot')
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none'
+  }
+  slideIndex++
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace('active', '')
+  }
+  slides[slideIndex - 1].style.display = 'block'
+  dots[slideIndex - 1].className += 'active'
+  setTimeout(showSlides, 2000)
+}
